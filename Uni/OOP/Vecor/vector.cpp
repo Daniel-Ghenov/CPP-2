58dcd3b280c12b54f8259dc317c85afcb2a48dfd
Daniel Ghenov <47058577+Daniel-Ghenov@users.noreply.github.com>
2023-04-04 23:24:16 +0300

    Vector Update

--- Uni/OOP/Vecor/vector.cpp
+++ Uni/OOP/Vecor/vector.cpp
@@ -1,10 +1,27 @@
-#include "vector.h"
+#include "Vector.h"
+
+template <class T>
+void Vector<T>::copyFrom(const Vector<T>& other){
+    data = new T[other.maxSize];
+    for(size_t i {0}; i < other.size; i++){
+        data[i] = other.data[i];
+    }
+    size = other.size;
+    maxSize = other.maxSize;
+}
 
 template <class T>
 Vector<T>::Vector(){
     data = nullptr;
     size = 0;
-    maxSize = 8;
+    maxSize = 4;
+}
+
+template <class T>
+void Vector<T>::clear(){
+    delete[] data;
+    data = nullptr;
+    size = maxSize = 0;
 }
 
 template <class T>
@@ -14,45 +31,65 @@ Vector<T>::Vector(size_t size, const T& fill){
     for(size_t i {0}; i < size;i++){
         data[i] = fill;
     }
+    if(size > maxSize){
+        maxSize = size * 2;
+    }
     
 }
 template <class T>
 Vector<T>::~Vector(){
-    clear();s
+    clear();
 }
+
 template <class T>
 Vector<T>& Vector<T>::operator=(const Vector<T>& other){
-    
+    if(this != &other){
+        clear();
+        copyFrom(other);
+    }
+    return *this;
 }
 template <class T>
 Vector<T>::Vector(const Vector<T>& other){
-    
+    copyFrom(other);
 }
 template <class T>
-void Vector<T>::app(T newData){
-    
+void Vector<T>::app(const T& newData){
+    if(data == nullptr){
+        maxSize = 4;
+        size++;
+        
+        
+    }
+    if(size == maxSize){
+        maxSize *= 2;
+        resize(maxSize);
+    }
+    data[size++] = newData;
 }
 template <class T>
 void Vector<T>::popback(){
-    
-}
-template <class T>
-void Vector<T>::clear(){
-    delete[] data;
-    data = nullptr;
-    size = maxSize = 0;
+    size--;    
 }
 
 template <class T>
 void Vector<T>::resize(size_t size){
-
+    resize(size, T());
 }
+
 template <class T>
-void Vector<T>::copyFrom(const Vector<T>& other){
-    data = new T[other.maxSize];
-    for(size_t i {0}; i < other.size; i++){
-        data[i] = other.data[i];
+void Vector<T>::resize(size_t size, const T& fill){
+    T* temp = new T [2 * size];
+    for(size_t i {0}; i < (size > this->size)? this->size : size; i++){
+        if(i > this->size){
+            temp[i] = fill;
+        }
+        else
+            temp[i] = data[i];
     }
-    size = otehr.size;
-    maxSize = other.maxSize;
+
+    clear();
+    this->size = size;
+    this->maxSize = 2 * size;
+    data = temp;
 }
