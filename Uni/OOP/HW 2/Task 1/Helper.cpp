73241d95a182828d52e8208a05fedf69293d6cd1
TheHotDoge <47058577+TheHotDoge@users.noreply.github.com>
2023-04-15 20:25:57 +0300

    SSO string and Fixed compilators

---
+++ Uni/OOP/HW 2/Task 1/Helper.cpp
@@ -0,0 +1,63 @@
+#include "Helper.h"
+
+int strcomp(const char* first, const char* second){
+    if(!first || !second)
+        throw std::logic_error("Cannot process nullptr");
+
+
+    for(size_t i {0};;i++){
+        if(first[i] < second[i]){
+            return -1;
+        }
+        if(first[i] > second[i]){
+            return 1;
+        }
+        if(first[i] == second[i] && first[i] == '\0')
+            return 0;
+    }
+
+}
+void strcopy(char* Destination, const char* Source){
+    if(!Destination || !Source)
+        throw std::logic_error("Cannot process nullptr");
+
+    for(size_t i {0};; i++){
+        Destination[i] = Source[i];
+        if(Source[i] == '\0')
+            return;
+    }
+
+}
+void strcopy_s(char* Destination ,size_t size, const char* Source){
+    if(!Destination || !Source)
+        throw std::logic_error("Cannot process nullptr");
+
+    for(size_t i {0}; i < size; i++){
+        Destination[i] = Source[i];
+        if(Source[i] == '\0')
+            return;
+    }
+    Destination[size - 1] = '\0';
+}
+void strconcat(char* Destination, const char* Source){
+    if(!Destination || !Source)
+        throw std::logic_error("Cannot process nullptr");
+
+    unsigned nullIdx = 0;
+    while(Destination[nullIdx++] != '\0');
+    nullIdx--;
+    for(size_t i {0} ;; i++){
+        Destination[i + nullIdx] = Source[i];
+        if(Source[i] == '\0')
+            return;
+    }
+
+}
+unsigned strleng(const char* str){
+    if(!str)
+        throw std::logic_error("Cannot process nullptr");
+    unsigned length = 0;
+    while(str[length++] != '\0');
+    return length - 1;
+}
+
