a76aa19ac366a69a55be395fba8e58e03f375bca
Daniel Ghenov <47058577+Daniel-Ghenov@users.noreply.github.com>
2023-04-15 23:04:44 +0300

    Revert "Revert "SSO string and Fixed compilators""
    This reverts commit b204969b60143dfa15c756da1aba494817d2da95.

---
+++ Uni/OOP/HW 2/Task 1/Helper.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+int strcomp(const char* first, const char* second);
+void strcopy_s(char* Destination,size_t size, const char* Source);
+void strcopy(char* Destination, const char* Source);
+void strconcat(char* Destination, const char* Source);
+unsigned strleng(const char* str);
