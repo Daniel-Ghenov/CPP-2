10be88edefb23847941cd78a07c9215f4f917879
TheHotDoge <47058577+TheHotDoge@users.noreply.github.com>
2023-05-05 17:35:26 +0300

    Editing Files Structure

--- Uni/UP/Projects/Project 2048 Old/Math.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-int pow(int a, int b);
-int len(int a);
-bool isEmptySpace(int** field, int fieldSize);
-void addRandomNumber(int** field, int* randomWeight, int fieldSize, int& turnCount);
-bool arePossibleMoves(int** field, int fieldSize);
