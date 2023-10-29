e91c103b3195e2ef1a00ed60fb8c5ae79cab3495
Daniel Ghenov <daniel.ghenov@protonmail.com>
2023-10-29 18:23:03 +0200

    Starting HW4 structure

---
+++ Uni/DSA/Homeworks/HW4/Cache/solution.cpp
@@ -0,0 +1,64 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+struct Node{
+    int key;
+    int data;
+    Node* next = nullptr;
+    Node* prev = nullptr;
+
+    Node(int key, int data);
+    Node(int key, int data, Node* next);
+    Node(int key, int data, Node* next, Node* prev);
+    Node(const Node& other);
+    Node& operator=(const Node& other);
+
+};
+
+Node::Node(int key, int data){
+    this->key = key;
+    this->data = data;
+}
+
+Node::Node(const Node& other){
+    this->key = other.key;
+    this->data = other.data;
+}
+
+Node::Node(int key, int data, Node* next){
+    this->key = key;
+    this->data = data;
+    this-> next = next;
+}
+
+Node::Node(int key, int data, Node* next, Node* prev){
+    this->key = key;
+    this->data = data;
+    this->next = next;
+    this->prev = prev;
+}
+
+class List{
+private:
+    Node* head = nullptr;
+    Node* tail = nullptr;
+    int overHeatNum;
+    int currOverHeat = 0;
+
+public:
+
+    void put(int key, int data);
+    void get(int key);
+
+    void pop_back();
+};
+
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
