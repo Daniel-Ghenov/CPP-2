8a98ff5a0c841634c933e21cf2697d940fcce390
Daniel Ghenov <daniel.ghenov@protonmail.com>
2023-12-17 15:32:58 +0200

    Starting HW 11 Breakout Done

--- Uni/DSA/Homeworks/HW 11/Tunnels/solution.cpp
+++ Uni/DSA/Homeworks/HW 11/Tunnels/solution.cpp
@@ -1,3 +1,49 @@
-//
-// Created by PC-Admin on 17.12.2023 г..
-//
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+const long long INF = LONG_LONG_MAX;
+
+struct Edge {
+    int weight;
+    int to;
+    int kgs;
+
+    Edge(int weight, int to, int kgs) : weight(weight), to(to), kgs(kgs) {}
+
+    bool operator<(const Edge& other) const {
+        if(weight == other.weight) {
+            if(to == other.to) {
+                return kgs < other.kgs;
+            }
+            return to < other.to;
+        }
+        return weight < other.weight;
+    }
+
+    bool operator==(const Edge& other) const {
+        return weight == other.weight && to == other.to && kgs == other.kgs;
+    }
+
+};
+
+int minKgsToReach(const std::vector<std::vector<Edge>>& graph, int battery) {
+
+}
+
+int main() {
+
+    int n, m, k;
+    std::cin >> n >> m >> k;
+    std::vector<std::vector<Edge>> graph(n);
+
+    for(int i = 0; i < m; i++) {
+        int from, to, weight, kgs;
+        std::cin >> from >> to >> kgs >> weight;
+        graph[from - 1].emplace_back(weight, to - 1, kgs);
+    }
+
+    return 0;
+}
