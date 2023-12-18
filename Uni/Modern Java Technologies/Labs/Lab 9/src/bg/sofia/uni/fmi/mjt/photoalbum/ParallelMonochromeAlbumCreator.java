9cb5916bd2d7396ae8f1ab89585ee8cf0ed4e292
Daniel Ghenov <daniel.ghenov@protonmail.com>
2023-12-18 23:18:53 +0200

    Fixing Lab 9

--- Uni/Modern Java Technologies/Labs/Lab 9/src/bg/sofia/uni/fmi/mjt/photoalbum/ParallelMonochromeAlbumCreator.java
+++ Uni/Modern Java Technologies/Labs/Lab 9/src/bg/sofia/uni/fmi/mjt/photoalbum/ParallelMonochromeAlbumCreator.java
@@ -1,5 +1,7 @@
 package bg.sofia.uni.fmi.mjt.photoalbum;
 
+import bg.sofia.uni.fmi.mjt.photoalbum.image.Image;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -17,9 +19,13 @@ public class ParallelMonochromeAlbumCreator implements MonochromeAlbumCreator {
 
     private static final int THREADS_TO_VIRTUAL_MULTIPLIER = 3;
 
+    private static final Set<String> SUPPORTED_EXTENSIONS = Set.of("jpeg", "jpg", "png");
+
     final int processorsCount;
 
     final ImageQueue imageQueue;
+
+    volatile boolean isFinished = false;
 
     public ParallelMonochromeAlbumCreator(int processorsCount) {
         this.processorsCount = processorsCount;
@@ -28,9 +34,14 @@ public class ParallelMonochromeAlbumCreator implements MonochromeAlbumCreator {
 
     @Override
     public void processImages(String sourceDirectory, String outputDirectory) {
+        isFinished = false;
         Set<Path> toVisit = getToVisit(sourceDirectory);
         Path destination = Path.of(outputDirectory);
-
+        try {
+            Files.createDirectories(destination);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         List<Thread> producers = Collections.nCopies(processorsCount * THREADS_TO_VIRTUAL_MULTIPLIER,
                                                       Thread.ofVirtual().start(getProducerTask(toVisit)
         ));
@@ -44,6 +55,9 @@ public class ParallelMonochromeAlbumCreator implements MonochromeAlbumCreator {
         return () -> {
             while (!toVisit.isEmpty()) {
                 Path next = getPath(toVisit);
+                if (!isValidPath(next)) {
+                    continue;
+                }
                 Image img = loadImage(next);
                 imageQueue.add(img);
             }
@@ -52,7 +66,7 @@ public class ParallelMonochromeAlbumCreator implements MonochromeAlbumCreator {
 
     private Runnable getConsumerTask(Path destination) {
         return () -> {
-            while (!imageQueue.isEmpty()) {
+            while (!isFinished  || !imageQueue.isEmpty()) {
                 Image toProcess = imageQueue.get();
                 Image monochromeImg = convertToBlackAndWhite(toProcess);
                 writeImageToFile(monochromeImg, destination);
@@ -60,13 +74,14 @@ public class ParallelMonochromeAlbumCreator implements MonochromeAlbumCreator {
         };
     }
 
-    private static void terminateExecution(List<Thread> producers, List<Thread> consumers) {
+    private void terminateExecution(List<Thread> producers, List<Thread> consumers) {
         try {
             for (Thread producer : producers) {
                 producer.join();
             }
+            isFinished = true;
             for (Thread consumer : consumers) {
-                consumer.interrupt();
+                consumer.join();
             }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
@@ -107,22 +122,31 @@ public class ParallelMonochromeAlbumCreator implements MonochromeAlbumCreator {
     }
 
     private Image convertToBlackAndWhite(Image image) {
-        BufferedImage processedData = new BufferedImage(image.data.getWidth(),
-                                                        image.data.getHeight(),
+        BufferedImage processedData = new BufferedImage(image.getData().getWidth(),
+                                                        image.getData().getHeight(),
                                                         BufferedImage.TYPE_BYTE_GRAY);
-        processedData.getGraphics().drawImage(image.data, 0, 0, null);
+        processedData.getGraphics().drawImage(image.getData(), 0, 0, null);
 
-        return new Image(image.name, processedData);
+        return new Image(image.getName(), processedData);
     }
 
     private void writeImageToFile(Image image, Path output) {
         try {
-            BufferedImage bufferedImage = image.data;
+            BufferedImage bufferedImage = image.getData();
 
-            ImageIO.write(bufferedImage, "jpg", output.resolve(image.name).toFile());
+            ImageIO.write(bufferedImage, image.getExtension(), output.resolve(image.getName()).toFile());
 
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
+    }
+
+    private boolean isValidPath(Path path) {
+        if (path == null) {
+            return false;
+        }
+        String extension = path.toString().lastIndexOf(".") == -1 ? "" :
+                path.toString().substring(path.toString().lastIndexOf(".") + 1);
+        return SUPPORTED_EXTENSIONS.contains(extension);
     }
 }
