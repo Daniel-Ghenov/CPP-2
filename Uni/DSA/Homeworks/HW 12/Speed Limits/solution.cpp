845bccc6fab0f264478f1fd2ed44a41b8229922c
Daniel Ghenov <daniel.ghenov@protonmail.com>
2023-12-31 14:36:11 +0200

    Final Working Commit

--- Uni/DSA/Homeworks/HW 12/Speed Limits/solution.cpp
+++ Uni/DSA/Homeworks/HW 12/Speed Limits/solution.cpp
@@ -1,74 +1,13 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include "DisjointSet.h"
 
 
-class DisjointSet {
-private:
-    std::vector<long long> predecessors;
-    std::vector<long long> sizes;
+typedef std::pair<long long, long long> lPair;
 
-public:
-
-    explicit DisjointSet(long long size);
-
-    void connect(long long, long long);
-
-    bool connected(long long, long long);
-
-private:
-    long long predecessor(long long);
-};
-
-
-DisjointSet::DisjointSet(long long size): predecessors(size), sizes(std::vector<long long>(size, 0)) {
-    for (long long i = 0; i < size; ++i) {
-        predecessors[i] = i;
-    }
-}
-
-void DisjointSet::connect(long long a, long long b) {
-    if(a == b) {
-        return;
-    }
-
-    if(a < 0 || b < 0 || a >= predecessors.size() || b >= predecessors.size()) {
-        throw std::out_of_range("Index out of range");
-    }
-
-    long long aPredecessor = predecessor(a);
-    long long bPredecessor = predecessor(b);
-
-    if(sizes[aPredecessor] > sizes[bPredecessor]) {
-        std::swap(aPredecessor, bPredecessor);
-    }
-
-    predecessors[aPredecessor] = predecessors[bPredecessor];
-    if(sizes[aPredecessor] == sizes[bPredecessor]) {
-        sizes[aPredecessor]++;
-    }
-}
-
-bool DisjointSet::connected(long long a, long long b) {
-    if(a == b) {
-        return true;
-    }
-
-    if(a < 0 || b < 0 || a >= predecessors.size() || b >= predecessors.size()) {
-        throw std::out_of_range("Index out of range");
-    }
-
-    return predecessor(a) == predecessor(b);
-}
-
-long long DisjointSet::predecessor(long long a) {
-    if(predecessors[a] == a) {
-        return a;
-    }
-    long long predecess = predecessor(predecessors[a]);
-    predecessors[a] = predecess;
-    return predecess;
-}
+const long long INF = 1e18;
+const long long NEG_INF = -1e18;
 
 struct Edge {
     long long from;
@@ -82,7 +21,7 @@ struct Edge {
     }
 };
 
-std::pair<long long, long long> getMinMaxSpeed(std::vector<Edge>& edges, long long vertices, long long min) {
+lPair getMinMaxSpeed(std::vector<Edge>& edges, long long vertices, long long min) {
     DisjointSet set(vertices);
     long long minSpeed = edges[min].speed;
     long long maxSpeed = 0;
@@ -101,20 +40,22 @@ std::pair<long long, long long> getMinMaxSpeed(std::vector<Edge>& edges, long lo
             break;
         }
     }
+    if(currUsed < vertices) {
+        return {NEG_INF, INF};
+    }
     return {minSpeed, maxSpeed};
 }
 
-long long calculateDist(const std::pair<long long, long long>& pair) {
+long long calculateDist(const lPair& pair) {
     return pair.second - pair.first;
 }
 
-std::pair<long long, long long> getMinMaxSpeed(std::vector<Edge>& edges, long long vertices) {
-    DisjointSet set(vertices);
+lPair getMinMaxSpeed(std::vector<Edge>& edges, long long vertices) {
     std::sort(edges.begin(), edges.end());
-    std::pair<long long, long long> currMinDist = {edges.begin()->speed , edges.rbegin()->speed};
+    lPair currMinDist = {edges.begin()->speed , edges.rbegin()->speed};
 
-    for(long long i = 0; i < edges.size() - vertices; i++) {
-        auto newMinDist = getMinMaxSpeed(edges, vertices, i);
+    for(long long i = 0; i < edges.size() - vertices + 1; i++) {
+        lPair newMinDist = getMinMaxSpeed(edges, vertices, i);
         if(calculateDist(newMinDist) < calculateDist(currMinDist)) {
             currMinDist = newMinDist;
         }
